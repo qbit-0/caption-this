52065e50a6350675d531e4500eb35adff033c423
Duy Pham <duypham12241999@gmail.com>
2022-07-19 21:11:37 -0700

    Add heroku config

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Testing 123!");
+});
+
+app.listen(port, () => {
+    console.log(`listening on http://localhost:${port}`);
+});
